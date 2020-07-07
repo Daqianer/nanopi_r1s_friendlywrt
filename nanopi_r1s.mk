65d9011d12eaeaf44082ccd39fe4134ded6b052c
Daqianer <43440382+Daqianer@users.noreply.github.com>
2020-07-07 20:50:12 +0800

    init

---
+++ nanopi_r1s.mk
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+. device/friendlyelec/h5/base.mk
+TARGET_SD_RAW_FILENAME=FriendlyWrt_$(date +%Y%m%d)_NanoPi-R1S_aarch64_sd.img
+TARGET_EFLASHER_RAW_FILENAME=FriendlyWrt_$(date +%Y%m%d)_NanoPi-R1S_aarch64_eflasher.img
+TARGET_FRIENDLYWRT_CONFIG=my_config1
+
